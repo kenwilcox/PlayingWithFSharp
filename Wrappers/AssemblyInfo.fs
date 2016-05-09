82fcf7cf4244dad01666f22cacde0dec070255ff
Ken Wilcox <wilcoxk@medamerica.com>
2016-05-09 16:50:23 -0600

    Added Wrapper

---
+++ Wrappers/AssemblyInfo.fs
@@ -0,0 +1,41 @@
+﻿namespace Wrappers.AssemblyInfo
+
+open System.Reflection
+open System.Runtime.CompilerServices
+open System.Runtime.InteropServices
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[<assembly: AssemblyTitle("Wrappers")>]
+[<assembly: AssemblyDescription("")>]
+[<assembly: AssemblyConfiguration("")>]
+[<assembly: AssemblyCompany("")>]
+[<assembly: AssemblyProduct("Wrappers")>]
+[<assembly: AssemblyCopyright("Copyright ©  2016")>]
+[<assembly: AssemblyTrademark("")>]
+[<assembly: AssemblyCulture("")>]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[<assembly: ComVisible(false)>]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[<assembly: Guid("061bf6cb-d0c6-4158-9dde-d327d798c0aa")>]
+
+// Version information for an assembly consists of the following four values:
+// 
+//       Major Version
+//       Minor Version 
+//       Build Number
+//       Revision
+// 
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [<assembly: AssemblyVersion("1.0.*")>]
+[<assembly: AssemblyVersion("1.0.0.0")>]
+[<assembly: AssemblyFileVersion("1.0.0.0")>]
+
+do
+    ()
---
+++ Wrappers/Program.fs
@@ -0,0 +1,25 @@
+﻿// Learn more about F# at http://fsharp.org
+// See the 'F# Tutorial' project for more help.
+
+[<EntryPoint>]
+let main argv = 
+    printfn "%A" argv
+
+    let add1 input = input + 1
+    let times2 input = input * 2
+
+    let genericLogger anyFunc input =
+      printfn "input is %A" input
+      let result = anyFunc input
+      printfn "result is %A" input
+      result
+
+    let add1WithLogging = genericLogger add1
+    let times2WithLogging = genericLogger times2
+
+    add1WithLogging 3 |> ignore
+    times2WithLogging 3 |> ignore
+
+    [1..5] |> List.map add1WithLogging |> ignore
+
+    0 // return an integer exit code
