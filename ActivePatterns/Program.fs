1ae0dfb058d30b7eff628831f33453a7a97ffc39
Ken Wilcox <wilcoxk@medamerica.com>
2016-09-22 08:46:33 -0600

    added regex example

--- ActivePatterns/Program.fs
+++ ActivePatterns/Program.fs
@@ -1,8 +1,9 @@
 ﻿// Learn more about F# at http://fsharp.org
 // See the 'F# Tutorial' project for more help.
+open System.Text.RegularExpressions
 
 [<EntryPoint>]
-let main argv = 
+let main argv =
     printfn "%A" argv
 
     // create some active patterns
@@ -26,5 +27,21 @@ let main argv =
     testParse "12"
     testParse "true"
     testParse "abc"
+
+    // regex example
+    let (|FirstRegexGroup|_|) pattern input =
+      let m = Regex.Match(input, pattern)
+      if (m.Success) then Some m.Groups.[1].Value else None
+
+    let testRegex str =
+      match str with
+      | FirstRegexGroup "http://(.*?)/(.*)" host ->
+            printfn "The value is a url and the host is %s" host
+      | FirstRegexGroup ".*?@(.*)" host ->
+            printfn "The value is an email and the host is %s" host
+      | _ -> printfn "The value '%s' is something else" str
+
+    testRegex "http://example.com/test"
+    testRegex "sue@example.com"
 
     0 // return an integer exit code
