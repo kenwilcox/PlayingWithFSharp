7efec6dbb96cbee4dfa64105a40c54dd11cd2b9f
Ken Wilcox <wilcoxk@medamerica.com>
2016-09-22 08:59:13 -0600

    printf and printfn... got it

--- ActivePatterns/Program.fs
+++ ActivePatterns/Program.fs
@@ -44,4 +44,18 @@ let main argv =
     testRegex "http://example.com/test"
     testRegex "sue@example.com"
 
+    // how about fizzbuzz
+    let (|MultOf3|_|) i = if i % 3 = 0 then Some MultOf3 else None
+    let (|MultOf5|_|) i = if i % 5 = 0 then Some MultOf5 else None
+
+    let fizzBuzz i =
+      match i with
+      | MultOf3 & MultOf5 -> printf "FizzBuzz, "
+      | MultOf3 -> printf "Fizz, "
+      | MultOf5 -> printf "Buzz, "
+      | _ -> printf "%i, " i
+
+    [1..20] |> List.iter fizzBuzz
+    printfn ""
+
     0 // return an integer exit code
