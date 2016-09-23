b4c873c7cf8c8fe409a5e7e33f0efe7f9c4a70f1
Ken Wilcox <wilcoxk@medamerica.com>
2016-09-23 16:17:35 -0600

    Added Immutability

---
+++ Immutability/Program.fs
@@ -0,0 +1,27 @@
+﻿// Learn more about F# at http://fsharp.org
+// See the 'F# Tutorial' project for more help.
+
+type PersonName = {FirstName: string; LastName:string}
+
+[<EntryPoint>]
+let main argv =
+    printfn "%A" argv
+
+    let list = [1;2;3;4]
+
+    // prepend to make a new List
+    let list2 = 0::list
+
+    // get the last 4
+    let list3 = list2.Tail
+
+    // the two lists are the identical object in memory
+    printfn "%A" (System.Object.ReferenceEquals(list, list3))
+
+    let john = {FirstName="John"; LastName="Doe"}
+    printfn "%A" john
+
+    let alice = {john with FirstName="Alice"}
+    printfn "%A" alice
+
+    0 // return an integer exit code
